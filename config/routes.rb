1e2d3734b20ce5c0fa8ab671d9117f75cf709d28
Patryk Stępień <droznyk@gmail.com>
2017-05-27 10:04:25 +0200

    set events#index as root for logged users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root 'home#index', as: :authenticated_root
+      root 'events#index', as: :authenticated_root
     end
 
     unauthenticated do

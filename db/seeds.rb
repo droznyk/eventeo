683ad20426a1c42168d7e92d0208b9dc17e172a9
Patryk Stępień <droznyk@gmail.com>
2017-05-27 10:07:48 +0200

    add useful data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+admin = User.create(email: 'admin@sellito.pl', password: 'password')
+user = User.create(email: 'test@test.pl', password: 'password')
+
+20.times do |i|
+  admin.events.create(title: "Admin event no. #{i}")
+  user.events.create(title: "User event no. #{i}")
+end
